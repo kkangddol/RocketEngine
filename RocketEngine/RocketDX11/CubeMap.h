5a6c660d668a6faca511c932daa209553afde883
kkangddol_KGIT <knemo333@gmail.com>
2024-03-12 20:59:45 +0900

    큐브맵 클래스만 만들어서 커밋.

---
+++ RocketEngine/RocketDX11/CubeMap.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <string>
+
+#include "MathHeader.h"
+
+namespace Rocket::Core
+{
+	class CubeMap
+	{
+	public:
+		CubeMap();
+		~CubeMap();
+
+		void SetTexture(std::wstring filePath);
+	};
+}
